8174e60870e4cdce586714a38ecca705d0d48ced
victorPRodrigues <victor.pe.rodrigues@gmail.com>
2020-07-25 20:05:49 -0300

    Adding classes and exceptions

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,31 @@
+package chess;
+
+import board.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instatiating ChessPosition. " +
+                    "Valid values: from a1 to h8");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - this.row, this.column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
